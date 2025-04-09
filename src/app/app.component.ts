ab2fef8936c3f59331926fa0d2e429085fead9f8
Eliah Gut <eliah.gut@swisscom.com>
2025-04-09 11:33:34 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { HobbiesComponent } from "./hobbies/hobbies.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutMeComponent, ProjectsComponent, SkillsComponent, HobbiesComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HeroComponent},
+    {path: '1', component: AboutMeComponent},
+    {path: '2', component: SkillsComponent},
+    {path: '3', component: ProjectsComponent},
+    {path: '4', component: HobbiesComponent},
+    {path: '5', component: ContactComponent}
+];
